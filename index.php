e76ba698f22930a09b965c2f52c5c7bbbe4b0c59
Cristian Blanco <cristianpblanco@gmail.com>
2022-11-13 18:31:56 -0500

    Carqgue inicial de proyecto v0.1

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 2</title>
+</head>
+
+<body>
+    <h1>Project PHP</h1>
+    <?php
+    /// uso de switch
+        $i=1;
+        switch($i) {
+            case 0:
+                echo "i es igual a cero <br>";
+                break;
+            case 1:
+                echo "i es igual a uno <br>";
+                break;
+            case 2:
+                echo "i es igual a dos";
+                break;
+        }
+    ?>
+
+</body>
+
+</html>
